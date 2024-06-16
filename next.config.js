294fe3d3da7362060602809cf55ae27e7ea84de0
slimanePLF <slimaneber2001@gmail.com>
2024-06-16 11:28:49 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: "export", // <=== enables static exports
   reactStrictMode: true,
-  basePath: "/portfolio",
+  basePath: "/portfolioo",
 };
 
 module.exports = nextConfig
